c60ff57b2e89bd4193cc731e79bcba4dfda4c898
oscar <oscariva1995@gmail.com>
2017-09-21 16:20:10 -0500

    Change settings.py

--- myexample/settings.py
+++ myexample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q$_+!!v$!s*%s9bdz!ra$3_^5(j$_gp-z3bu9f!$rvq5n+&g*o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
